41da4b7b8762c62e27a9feac14dddce73b1e9924
Devan Suresh <devansuresh05@gmail.com>
2025-11-28 22:52:15 +0530

    Bump container version in deployment

--- TODO app/src/server.js
+++ TODO app/src/server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (_, res) => {
-    res.send(`Server running on port ${PORT}`)
-})
+    res.send(`Server running on port ${PORT}`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
