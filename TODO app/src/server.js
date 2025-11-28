7cb5e1fde7c814334119f9423793ecd39dc99dd4
Devan Suresh <devansuresh05@gmail.com>
2025-11-28 23:21:00 +0530

    Add a static html page

--- TODO app/src/server.js
+++ TODO app/src/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (_, res) => {
-    res.send(`Server running on port ${PORT}`);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
