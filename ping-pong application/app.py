68d46c314683fc4c26db39ae501c426e928e33d3
Devan Suresh <devansuresh05@gmail.com>
2026-01-01 14:48:52 +0530

    feat: ping-pong now writes count to disk

--- ping-pong application/app.py
+++ ping-pong application/app.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from os import environ
 
 app = FastAPI()
 
-counter = 0
+FILE_PATH = environ.get('FILE_PATH', './count.txt')
+
+def write_to_disk(string):
+    with open(FILE_PATH, 'w') as file:
+        file.write(string)
+
+def read_from_disk():
+    with open(FILE_PATH, 'r') as file:
+        return file.read()
+
+write_to_disk('0')
+
 
 @app.get("/pingpong", response_class=PlainTextResponse)
 def get_counter():
-    global counter
+    counter = int(read_from_disk())
     counter += 1
+    write_to_disk(str(counter))
     return f"pong {counter}"
