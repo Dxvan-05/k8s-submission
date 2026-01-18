95f85cd8632ce2737b771154b9427ddc131b3afd
Devan Suresh <devansuresh05@gmail.com>
2026-01-18 23:06:24 +0530

    Fix parsing of env, add condition to check if the image already exists

--- TODO app/src/server.js
+++ TODO app/src/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
 const downloadNewImage = require('./utils/download-new-image')
+const fs = require('fs');
 
 
 const PORT = process.env.PORT || 8000;
 
-const IMAGE_REFRESH_INTERVAL_MIN = process.env.IMAGE_REFRESH_INTERVAL_MIN || 1;
+const IMAGE_REFRESH_INTERVAL_MIN = parseInt(process.env.IMAGE_REFRESH_INTERVAL_MIN) || 1;
 const IMAGE_DOWNLOAD_URL = process.env.IMAGE_DOWNLOAD_URL || 'https://picsum.photos/1200';
-const IMAGE_DOWNLOAD_PATH = process.env.IMAGE_DOWNLOAD_PATH || './public/image2.jpeg';
+const IMAGE_DOWNLOAD_PATH = process.env.IMAGE_DOWNLOAD_PATH || './public/image.jpeg';
 
+if (!fs.existsSync(IMAGE_DOWNLOAD_PATH)) {
+    downloadNewImage(IMAGE_DOWNLOAD_URL, IMAGE_DOWNLOAD_PATH)
+        .catch(err => console.error(err));
+}
 setInterval(() => downloadNewImage(IMAGE_DOWNLOAD_URL, IMAGE_DOWNLOAD_PATH), IMAGE_REFRESH_INTERVAL_MIN * 60 * 1000)
 
 
